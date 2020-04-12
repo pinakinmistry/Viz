0b026d17a0d216133ad03c8b5a575da82bc877df
Pinakin Mistry <pinakin.nmistry@gmail.com>
2020-04-12 10:13:46 -0400

    Circles

--- color-beads-d3/js/main.js
+++ color-beads-d3/js/main.js
@@ -2,7 +2,31 @@ import {
   select,
 } from 'd3';
 
+const width = document.body.clientWidth;
+const height = document.body.clientHeight;
+
 const svg = select('body')
   .append('svg')
-    .attr('width', document.body.clientWidth)
-    .attr('height', document.body.clientHeight);
+    .attr('width', width)
+    .attr('height', height);
+
+const margin = {
+  top: 20,
+  bottom: 20,
+  left: 20,
+  right: 20
+};
+
+const data = [1, 2, 3, 4, 5];
+
+const g = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+const circles = g.selectAll('circle')
+  .data(data);
+
+circles.enter()
+  .append('circle')
+    .attr('cx', d => d * 20)
+    .attr('cy', d => d * 20)
+    .attr('r', 10);
